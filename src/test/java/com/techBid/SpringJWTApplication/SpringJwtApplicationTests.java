a2f2c91f9eaca3897a69423ea9c27bedc5a032ba
Dheeraj Patel <dkremo34@gmail.com>
2023-01-16 22:06:40 +0530

    Initial commit

---
+++ src/test/java/com/techBid/SpringJWTApplication/SpringJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techBid.SpringJWTApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
